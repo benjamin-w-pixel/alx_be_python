d936485d7f1c4472ed42f739b62d9e837fbf1d05
Benjamin W. Pixel <benjamin.w.pixel@example.com>
2025-07-07 09:31:44 +0300

    feet: add reactangle area script

---
+++ python_introduction/rectangle_area.py
@@ -0,0 +1,12 @@
+# rectangle_area.py
+
+# Define two variables: length and width, with specified values.
+length = 10
+width = 5
+
+# Calculate the area of the rectangle using the formula Area = length * width
+# Store the result in a variable named area.
+area = length * width
+
+# Print the calculated area in the format: The area of the rectangle is: [area].
+print(f"The area of the rectangle is: {area}")
